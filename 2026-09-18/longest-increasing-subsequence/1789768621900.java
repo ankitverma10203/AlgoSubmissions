014ac520e9f6bfd16f962ea683744ac21bd605bf
Ankit Verma <43405291+ankitverma10203@users.noreply.github.com>
2026-09-19 03:27:04 +0530

    Add 2026-09-18/longest-increasing-subsequence/1789768621900.java

---
+++ 2026-09-18/longest-increasing-subsequence/1789768621900.java
@@ -0,0 +1,18 @@
+}
+            dp[i + 1] = max + 1;
+        for (int i = 1; i <= nums.length; i++) {
+
+        int ans = 0;
+            ans = Math.max(ans, dp[i]);
+        }
+
+        // System.out.println(Arrays.toString(dp));
+
+            }
+                }
+                    max = Math.max(dp[j + 1], max);
+                if (nums[j] < nums[i]) {
+
+            for (int j = i - 1; j >= 0; j--) {
+            int max = 0;
+        for (int i = 0; i < nums.length; i++) {
