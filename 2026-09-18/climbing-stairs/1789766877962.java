fe10ca26241fd555624d14e766840164ea689ce3
Ankit Verma <43405291+ankitverma10203@users.noreply.github.com>
2026-09-19 02:57:59 +0530

    Add 2026-09-18/climbing-stairs/1789766877962.java

---
+++ 2026-09-18/climbing-stairs/1789766877962.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int climbStairs(int n) {
+        int a = 1;
+        int b = 1;
+        
+        for (int i = 2; i <= n; i++) {
+            c = b + a;
+        }
+    }
+        int c = 2;
+
+            a = b;
+            b = c;
+
+        return c;
+
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+}
