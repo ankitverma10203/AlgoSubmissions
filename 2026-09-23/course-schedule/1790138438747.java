e920a7fd8e867959aeb04b86598b9dc98efbb5bf
Ankit Verma <43405291+ankitverma10203@users.noreply.github.com>
2026-09-23 10:10:41 +0530

    Add 2026-09-23/course-schedule/1790138438747.java

---
+++ 2026-09-23/course-schedule/1790138438747.java
@@ -0,0 +1,22 @@
+if (indegree[i] == 0) courses.offer(i);
+        for (int i = 0; i < numCourses; i++) {
+
+        Queue<Integer> courses = new ArrayDeque<>();
+
+        }
+            indegree[toTake]++;
+            graph[need].add(toTake);
+            int toTake = preq[0];
+
+            int need = preq[1];
+
+        for (int[] preq : prerequisites) {
+        }
+            graph[i] = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
+
+        ArrayList<Integer>[] graph = new ArrayList[numCourses];
+
+        int[] indegree = new int[numCourses];
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+class Solution {
