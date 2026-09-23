fda395ea5073b4dd8412526c86a16142ea39042f
Ankit Verma <43405291+ankitverma10203@users.noreply.github.com>
2026-09-23 09:44:47 +0530

    Add 2026-09-23/rotting-oranges/1790136884404.java

---
+++ 2026-09-23/rotting-oranges/1790136884404.java
@@ -0,0 +1,24 @@
+int i = cell[0];
+                int[] cell = rotten.poll();
+
+            while (size-- > 0) {
+            int size = rotten.size();
+
+        while (!rotten.isEmpty() && freshCount > 0) {
+
+        int time = 0;
+
+        if (freshCount == 0) return 0;
+
+        }
+            }
+                else if (grid[i][j] == 2) rotten.offer(new int[] {i, j});
+                if (grid[i][j] == 1) freshCount++;
+            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < m; i++) {
+
+        int n = grid[0].length;
+        int m = grid.length;
+        int freshCount = 0;
+
+        Queue<int[]> rotten = new ArrayDeque<>();
